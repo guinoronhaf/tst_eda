e88b07a8ccdbf940df136c2714d1520eeba43b29
Guilherme <guilhermenoronhaf@gmail.com>
2025-08-31 15:45:59 -0300

    rotacoes

--- src/rotacoes/Main.java
+++ src/rotacoes/Main.java
@@ -1,84 +1,237 @@
 package rotacoes;
 
+import java.util.*;
+
 public class Main {
 
     public static class Node {
 
-        public int v;
-        public Node parent;
-        public Node left;
-        public Node right;
+	int v;
+	Node parent;
+	Node left;
+	Node right;
 
-        public Node(int v) {
-            this.v = v;
-        }
-
+	public Node(int v) {
+	    this.v = v;
+	}
+    
     }
 
-    public static class BST {
+    public static class AVL {
 
-        private Node root;
-        private int size;
+	private Node root;
+	private int size;
 
-        public BST() {
-            this.root = null;
-            this.size = 0;
-        }
+	public boolean isEmpty() {
+	    return this.root == null;
+	}
 
-        //isEmpty
-        public boolean isEmpty() {
-            return this.root == null;
-        }
+	//balance
+	//========================
+	private int balance(Node node) {
+	    return height(node.left) - height(node.right);
+	}
+	//========================
+	
+	//pending
+	//========================
+	private boolean isLeftPending(Node node) {
+	    return balance(node) >= 2;
+	}
 
-        //add
-        private void add(Node current, int k) {
-            if (current == null) {
-                this.root = new Node(k);
-                return;
-            }
-            if (current.v > k) {
-                if (current.left == null) {
-                    Node newNode = new Node(k);
-                    current.left = newNode;
-                    newNode.parent = current;
-                    return;
-                }
-                add(current.left, k);
-            } else {
-                if (current.right == null) {
-                    Node newNode = new Node(k);
-                    current.right = newNode;
-                    newNode.parent = current;
-                    return;
-                }
-                add(current.right, k);
-            }
-        }
+	private boolean isRightPending(Node node) {
+	    return balance(node) <= -2;
+	}
+	//========================
+	
+	//rebalance
+	//========================
+	private void rebalance(Node node) {
+	    if (node != null) {
+		if (isLeftPending(node)) {
+		    int balanceLeft = balance(node.left);
+		    if (balanceLeft == -1) {
+			rotateLeft(node.left);
+			rotateRight(node);
+		    } else if (balanceLeft == 1) {
+			rotateRight(node);
+		    }
+		} else if (isRightPending(node)) {
+		    int balanceRight = balance(node.right);
+		    if (balanceRight == -1) {
+			rotateLeft(node);
+		    } else if (balanceRight == 1) {
+			rotateRight(node.right);
+			rotateLeft(node);
+		    }
+		}
+	    }
+	}
+	//========================
+	
+	//rebalanceUp
+	//========================
+	private void rebalanceUp(Node node) {
+	    if (node == null)
+		return;
+	    else {
+		rebalance(node);
+		rebalanceUp(node.parent);
+	    }
+	}
+	//========================
+	
+	//rotation
+	//========================
+	private void rotateLeft(Node node) {
+	    node.right.parent = node.parent;
+	    if (node.parent == null)
+		this.root = node.right;
+	    else if (node.parent.v > node.right.v)
+		node.parent.left = node.right;
+	    else
+		node.parent.right = node.right;
+	    node.parent = node.right;
+	    node.right = node.parent.left;
+	    node.parent.left = node;
+	    if (node.right != null)
+		node.right.parent = node;
+	    System.out.println("rot_esq(" + node.v + ")");
+	    System.out.println(Arrays.toString(this.preOrder().split(" ")));
+	}
 
-        public void add(int k) {
-            this.add(this.root, k);
-        }
+	private void rotateRight(Node node) {
+	    node.left.parent = node.parent;
+	    if (node.parent == null)
+		this.root = node.left;
+	    else if (node.parent.v > node.left.v)
+		node.parent.left = node.left;
+	    else
+		node.parent.right = node.left;
+	    node.parent = node.left;
+	    node.left = node.parent.right;
+	    node.parent.right = node;
+	    if (node.left != null)
+		node.left.parent = node;
+	    System.out.println("rot_dir(" + node.v + ")");
+	    System.out.println(Arrays.toString(this.preOrder().split(" ")));
+	}
+	//========================
 
-        //height
-        private int height(Node current) {
-            if (current == null)
-                return -1;
-            return 1 + Math.max(height(current.left), height(current.right));
-        }
+	//add
+	//========================
+	private void add(Node current, int k) {
+	    if (current.v > k) {
+		if (current.left == null) {
+		    Node newNode = new Node(k);
+		    current.left = newNode;
+		    newNode.parent = current;
+		    rebalanceUp(newNode);
+		    return;
+		}
+		add(current.left, k);
+	    } else {
+		if (current.right == null) {
+		    Node newNode = new Node(k);
+		    current.right = newNode;
+		    newNode.parent = current;
+		    rebalanceUp(newNode);
+		    return;
+		}
+		add(current.right, k);
+	    }
+	}
 
-        public int height() {
-            return this.height(this.root);
-        }
+	public void add(int k) {
+	    if (this.isEmpty())
+		this.root = new Node(k);
+	    else
+		this.add(this.root, k);
+	}
+	//========================
 
-        //size
-        public int size() {
-            return this.size;
-        }
+	//height
+	//========================
+	private int height(Node node) {
+	    if (node == null)
+		return -1;
+	    return 1 + Math.max(height(node.left), height(node.right));
+	}
 
+	public int height() {
+	    return this.height(this.root);
+	}
+	//========================
+	
+	//size
+	//========================
+	public int size() {
+	    return this.size;
+	}
+	//========================
+	
+	//preOrder
+	//========================
+	private String preOrder(Node current, String s) {
+	    if (current == null)
+		return s;
+	    else {
+		String newS = s + current.v + " ";
+		return preOrder(current.right, preOrder(current.left, newS));
+	    }
+	}
+
+	public String preOrder() {
+	    return this.preOrder(this.root, "");
+	}
+	//========================
+
+	@Override
+	public String toString() {
+	
+	    if (this.isEmpty())
+		return "";
+	    else {
+	
+		LinkedList<Node> queue = new LinkedList<>();
+		String avl = "";
+
+		queue.addLast(this.root);
+
+		while (!queue.isEmpty()) {
+		    Node current = queue.removeFirst();
+		    avl += current.v + " ";
+		    if (current.left != null)
+			queue.addLast(current.left);
+		    if (current.right != null)
+			queue.addLast(current.right);
+		}
+
+		return avl.substring(0, avl.length() - 1);
+
+	    }
+
+	}
+    
     }
 
     public static void main(String[] args) {
-        System.out.println("testando");
+
+	Scanner sc = new Scanner(System.in);
+
+	String line = sc.nextLine();
+	String[] values = line.split(" ");
+
+	AVL avl = new AVL();
+
+	for (int i = 0; i < values.length; i++)
+	    avl.add(Integer.parseInt(values[i]));
+
+	if (avl.toString().equals(line))
+	    System.out.println("balanceada");
+
+	sc.close();
+
     }
 
 }
